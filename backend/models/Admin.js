9e8137264f6b1936c7bf17c4cbd0f099a6c404dc
scalup275 <scalup275@gmail.com>
2026-02-17 14:32:41 +0530

    backend

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -21,7 +21,7 @@ const adminSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-// hash password before save
+// hash password before savewew
 adminSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
