e97969297819955f8e3200561330d32306464385
scalup275 <scalup275@gmail.com>
2026-02-18 00:57:02 +0530

    faaaa

--- backend/blogs/blog.routes.js
+++ backend/blogs/blog.routes.js
@@ -14,6 +14,6 @@ router.get("/", getAllBlogs);
 router.get("/:slug", getBlogBySlug);
 router.put("/:id", updateBlog);
 router.delete("/:id", deleteBlog);
-router.patch("/:id/toggle-publish", protectAdmin, togglePublish);
+router.patch("/:id/toggle-publish", togglePublish);
 
 export default router;
