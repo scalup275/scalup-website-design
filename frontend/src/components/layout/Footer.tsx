ffcadab88d2c4aabcbf7fdfa0949839075455064
scalup <scalup275@gmail.com>
2026-05-08 12:28:53 +0530

    Update Instagram link in Footer component

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -34,7 +34,7 @@ const socialLinks = [
   },
   {
     name: "Instagram (Agency)",
-    href: "https://instagram.com/scalup.agency",
+    href: "https://www.instagram.com/agency.scalup?igsh=MWkxM3J4eHJlbzEyaw%3D%3D&utm_source=qr",
     icon: Instagram,
   },
   {
