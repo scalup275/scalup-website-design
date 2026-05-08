c1d8cf0437ee1e4a955f987f9999b6b61669cb2e
scalup <scalup275@gmail.com>
2026-05-08 12:31:11 +0530

    Remove href from Instagram (Agency) link
    Removed the href for the Instagram (Agency) link.

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -34,7 +34,7 @@ const socialLinks = [
   },
   {
     name: "Instagram (Agency)",
-    href: "https://www.instagram.com/agency.scalup?igsh=MWkxM3J4eHJlbzEyaw%3D%3D&utm_source=qr",
+    href: "",
     icon: Instagram,
   },
   {
